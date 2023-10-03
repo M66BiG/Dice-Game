e9f6da76d6ecbaca4e963262aef36c3ac1f11ccd
Melih aus Beeck <melihesad@hotmail.de>
2023-10-03 03:01:43 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*
+         * Soll ein Programm werden welches random key input nimmt und dann random würfelt. Anschließend würfelt Gegner und dann wird verglichen. Score bis 3.
+         * Frage:
+         * Wie Random Number?
+         * Wie random Input nehmen.
+        */
+        Console.WriteLine("Hello, World!");
+    }
+}
